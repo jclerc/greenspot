13d4eaae32bed0c2f29d539c0698e97888822360
JClerc <jices25@gmail.com>
2016-11-07 09:43:20 +0100

    [menu:js] Move carousel class to new file

--- src/js/app/menu.js
+++ src/js/app/menu.js
@@ -1,69 +1,5 @@
 
-class Carousel {
-
-  constructor(target) {
-
-    var $target = $(target).eq(0);
-    var _this = this;
-
-    this.elements = {
-      container: $target,
-      slides: $target.find('.carousel__slides'),
-      items: $target.find('.carousel__item'),
-    };
-
-    this.count = this.elements.items.length;
-
-    this.elements.items.on('click', function (e) {
-      _this.goto($(this).index());
-      e.preventDefault();
-    });
-
-    this.elements.container.on('swipeLeft', function (e) {
-      _this.next();
-      e.preventDefault();
-    });
-
-    this.elements.container.on('swipeRight', function (e) {
-      _this.prev();
-      e.preventDefault();
-    });
-
-    this.index = 0;
-
-  }
-
-  prev() {
-    if (this.index > 0) {
-      this.goto(this.index - 1);
-    }
-  }
-
-  next() {
-    if (this.index < this.count - 1) {
-      this.goto(this.index + 1);
-    }
-  }
-
-  goto(index) {
-    if (index < 0 || index >= this.count) {
-      throw new Error('Index ' + index + ' is out of bounds');
-    } else if (index !== this.index) {
-      var width = this.elements.slides.width();
-      this.elements.slides.css('transform', 'translateX(' + -width * index + 'px)');
-      this.elements.items.removeClass('carousel__item_active');
-      this.elements.items.eq(index).addClass('carousel__item_active');
-      this.index = index;
-    }
-  }
-
-  item() {
-    return this.elements.items.eq(this.index);
-  }
-
-}
-
-(() => {
+App.ready($ => {
   'use strict';
 
   var carousel = new Carousel('.carousel');
@@ -83,4 +19,4 @@ class Carousel {
     $('.page_menu').show();
   });
 
-})();
+});
